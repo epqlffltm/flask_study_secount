07ceeae10a23d33f547a35159887f6f60be879be
epqlffltm <dsfake1225@gmail.com>
2025-09-08 22:10:30 +0900

    9-8

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route("/user/<username>")
+def user(username):
+    return f"user{username}"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#디버그 모드, 실제운영시엔 비활성화
